dc79e5681445b7bbe1c92c9f6216cc3400f4c9d7
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-02-11 13:16:35 +0200

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
